3d46746cd5ea52d12a3365e262722bfb33574698
Grégory MEBARKI <gregory.mebarki@gmail.com>
2020-05-16 14:22:13 +0200

    rm dist folder and change vue.config.js for production

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+/*module.exports = {
     devServer: {
       proxy: 'http://localhost:3000/api'
     }
   }
-
+*/
+  module.exports = {
+    configureWebpack: config => {
+      if (process.env.NODE_ENV === 'production') {
+        proxy: 'http://appnodejs.mebdesign.fr/api'
+      } else {
+        devServer: {
+          proxy: 'http://localhost:3000/api'
+        }
+      }
+    }
+  }
