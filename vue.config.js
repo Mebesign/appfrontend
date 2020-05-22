7349d52c1afb85c406ac2c430407b29d1c581e19
Grégory MEBARKI <gregory.mebarki@gmail.com>
2020-05-22 19:22:51 +0200

    test 127.0.0.1  change config api with full url

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
   module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? 'http://localhost:3000/api'
-      : 'http://appnodejs.mebdesign.fr/api'
+      : 'http://127.0.0.1/api'
   }
