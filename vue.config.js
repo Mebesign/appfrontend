1a5b7d6156309400404de2097e6722dbb6f9f9d5
Grégory MEBARKI <gregory.mebarki@gmail.com>
2020-05-23 10:15:02 +0200

    rm for prod vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
 
-/*module.exports = {
+module.exports = {
   devServer: {
     proxy: 'http://localhost:3000/api'
   }
-}*/
-
-/*
-  module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? 'http://localhost:3000/api'
-      : 'http://127.0.0.1/api'
-  }*/
+}
