3237df47cd0544db5dbf4fcba9c7d2f9da1cb6ea
Grégory MEBARKI <gregory.mebarki@gmail.com>
2020-05-23 09:55:57 +0200

    test ...

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
+
+/*module.exports = {
+  devServer: {
+    proxy: 'http://localhost:3000/api'
+  }
+}*/
+
+/*
   module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? 'http://localhost:3000/api'
       : 'http://127.0.0.1/api'
-  }
+  }*/
